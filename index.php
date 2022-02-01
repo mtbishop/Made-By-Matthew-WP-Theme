966bd4f7f4c0a15d1e5de1e2ac4f334ece7d7924
mtbishop <bishopmatthew76@gmail.com>
2022-02-01 00:26:28 -0700

    head fixed

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <?php
+    wp_head();
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +16,10 @@
     <header>
         <h1>testing123</h1>
     </header>
+
+    <?php
+    wp_footer();
+    ?>
 
 </body>
 
